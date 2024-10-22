89b372d221d7865b13f48aed339c17382218a17a
clara :)) <clara.turro01@estudiant.upf.edu>
2024-10-22 12:43:46 +0200

    Change the divisors function
    Previously, we were returning the full list of divisors,
    and now we just return the number of them.

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,12 +1,11 @@
-def divisors(x):
-	divisors_x = []
+def num_divisors(x):
 	i = 0
-        
+	n = 0
 	while i < x:
 		i = i + 1
 		if x % i == 0:
-			 divisors_x.append(i)
-	return(divisors_x)
+			n = n + 1
+	return(n)
 
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
@@ -14,27 +13,27 @@ def divisors(x):
 def main(x):
 	## YOU CODE SHOULD START HERE AT THE SAME
 	## IDENTATION AS THIS COMMENT
-	
+
 	# 1. Find the divisors of x and count them
-	divisors_x = divisors(x)
+	divisors_x = num_divisors(x)
 
 	# 2. Find the divisors of all the smaller positive integrers of x and count them
 	i = 0
 	n = 0
 	
 	while i < x-1:
-		i = i + 1		
-		divisors_i = divisors(i)	
-		if len(divisors_i) >= len(divisors_x):
+		i = i + 1
+		divisors_i = num_divisors(i)
+		if divisors_i >= divisors_x:
 			n = n + 1
-		
+
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	
-	if n == 0:	
+
+	if n == 0:
 		return("anti-prime")
 	else:
 		return("not anti-prime")
